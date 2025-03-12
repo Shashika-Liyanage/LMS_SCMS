ffc9b87ce13e9c1db9842f546ea313168d8d4846
Shashika-Liyanage <nayanamini2001@gmail.com>
2025-03-12 22:39:58 +0530

    Folders and sidebar (Student, Admin, Lecturer)

---
+++ src/Components/Layouts/Lecturer_Layout/LecturerL.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const LecturerL = () => {
+  return (
+    <>
+      <Outlet /> 
+    </>
+  );
+}
+
+
+export default LecturerL;
---
+++ src/Components/Pages/AdminLMS/Admin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdminSidebar from './AdminSidebar/AdminSidebar'
+
+function Admin() {
+  return (
+    <AdminSidebar/>
+  )
+}
+
+export default Admin
